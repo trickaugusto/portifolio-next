d71e2e5540a530f7d01f73e737c76ab086e4f699
André Melo <andrelucas@zhavia.com.br>
2023-12-13 09:16:55 -0300

    feat: inserted Head informations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,30 @@
-import { Roboto } from 'next/font/google'
-import '@/styles/globals.css'
+import { Roboto } from 'next/font/google';
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 const roboto = Roboto({
   weight: ['100', '400', '500', '900'],
   subsets: ['latin'],
-})
-  
+});
+
+export const metadata = {
+  title: 'Trick Augusto',
+  description: 'Portfolio by @trickaugusto',
+};
+
 export default function App({ Component, pageProps }) {
   return (
-    <main className={roboto.className}>
-      <Component {...pageProps}/>
-    </main>
-  )
+    <>
+      <Head>
+        <title>{metadata.title || 'Next.js App'}</title>
+        <meta
+          name='description'
+          content={metadata.description || 'Next.js App Description'}
+        />
+      </Head>
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
